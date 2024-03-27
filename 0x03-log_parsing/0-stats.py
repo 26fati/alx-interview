a1ee2279d0ba4ee66a8c9e98d2888f1c6e768635
26fati <fatibeautiful1@gmail.com>
2024-03-27 10:43:17 +0100

    pycodestyle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """Log parsing"""
+from sys import stdin
 
 
-def print_line(dect, size):           
+def print_lines(dect, size):
+    ''' print lines'''
     print(f"File size: {size}")
     for key, value in dect.items():
         if value != 0:
             print(f"{key}: {value}")
 
 
-from sys import stdin
 errors = []
-count  = 0
+count = 0
 size = 0
-stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+stats = {'200': 0,
+         '301': 0,
+         '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 try:
     for line in stdin:
         line_parsed = line.split()[::-1]
@@ -28,11 +31,6 @@ try:
             if count == 10:
                 print_line(stats, size)
                 count = 0
-        
+
 finally:
     print_line(stats, size)
-
-
-
-
-
